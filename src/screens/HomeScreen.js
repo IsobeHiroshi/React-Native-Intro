8e262818be8db25e2109578e66bdebff0f0129e8
Hiroshi Isobe <isobe69b@gmail.com>
2022-04-30 11:06:40 -0700

    Complete counter screen and image screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,42 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      {/* Button element is not very customizable, self-closing */}
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Components Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("List")}
+        title="Go to List Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("Image")}
+        title="Go to Image Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("Counter")}
+        title="Go to Counter Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("Color")}
+        title="Go to Color Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("Square")}
+        title="Go to Square Demo"
+      />
+      {/* <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,31 +3,42 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" /* , key: "1" */ }, // Key should be string and unique
-    { name: "Friend #2" /* , key: "2" */ },
-    { name: "Friend #3" /* , key: "3" */ },
-    { name: "Friend #4" /* , key: "4" */ },
-    { name: "Friend #5" /* , key: "5" */ },
-    { name: "Friend #6" /* , key: "6" */ },
-    { name: "Friend #7" /* , key: "7" */ },
-    { name: "Friend #8" /* , key: "8" */ },
-    { name: "Friend #9" /* , key: "9" */ },
+    // Key should be string and unique
+    { name: "Friend #1", age: 20 /* , key: "1" */ },
+    { name: "Friend #2", age: 45 /* , key: "2" */ },
+    { name: "Friend #3", age: 32 /* , key: "3" */ },
+    { name: "Friend #4", age: 27 /* , key: "4" */ },
+    { name: "Friend #5", age: 53 /* , key: "5" */ },
+    { name: "Friend #6", age: 53 /* , key: "6" */ },
+    { name: "Friend #7", age: 30 /* , key: "7" */ },
+    { name: "Friend #8", age: 41 /* , key: "8" */ },
+    { name: "Friend #9", age: 23 /* , key: "9" */ },
   ];
 
   //   friends.element === {item: {name: 'Friend #1'}, index: 0}
 
   return (
     <FlatList
+      /* horizontal
+      showsHorizontalScrollIndicator={false} */
       keyExtractor={(friend) => friend.name} // Extract key
       data={friends}
       renderItem={({ item }) => {
         //Destructing
-        return <Text>{item.name}</Text>;
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        );
       }}
     />
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50,
+  },
+});
 
 export default ListScreen;
